7b525c12f3c5a27237a50f262e9d405444a013cd
Flávio Micheletti <flaviomicheletti@hotmail.com>
2012-10-30 13:11:29 -0200

    um monte de coisas

--- php/index.php
+++ php/index.php
@@ -140,15 +140,60 @@
             <div class="row">
                 <div class="span10 offset1">
                     <div class="furia-tree">
-                        <h5>PHP</h5>
-                        <div id="treecontrol" style="display: block;">
-                            <a href="#" title="Collapse the entire tree below"><img alt="-" src="../imagens/minus.gif">Contrair todos</a>
-                            <a href="#" title="Expand the entire tree below"><img alt="+" src="../imagens/plus.gif">Expandir todos</a>
+                        <div id="treecontrol">
+                            <a href="#" title="Collapse the entire tree below"><img alt="-" src="../furia/treeview/images/minus.gif">Contrair todos</a>
+                            <a href="#" title="Expand the entire tree below"><img alt="+" src="../furia/treeview/images/plus.gif">Expandir todos</a>
                         </div>
 
-                        <ul class="filetree treeview" id="browser">
-                            <li class="collapsable lastCollapsable"><div class="hitarea collapsable-hitarea lastCollapsable-hitarea"></div><span class="folder">Nível 0</span>
-                                <ul style="display: block;">
+                        <h6>PHP</h6>
+                        <!--                        <ul class="filetree treeview" id="browser">
+                                                    <li class="collapsable lastCollapsable"><div class="hitarea collapsable-hitarea lastCollapsable-hitarea"></div><span class="folder">Nível 0</span>
+                                                        <ul>
+                                                            <li><a href="nivel-zero/um-bom-comeco" class=""><span class="file">Um bom começo</span></a></li>
+                                                            <li><a href="nivel-zero/logica-de-programacao" class=""><span class="file">Lógica de Programação</span></a></li>
+                                                            <li><a href="nivel-zero/antes-de-enfiar-o-pe-na-jaca" class=""><span class="file">Antes de enfiar o pé na jáca</span></a></li>
+                                                            <li><a href="nivel-zero/intalando-o-ambiente" class=""><span class="file">Instalando o ambiente no estilo NEXT, NEXT, NEXT</span></a></li>
+                                                            <li><a href="nivel-zero/estudos-de-logica-1-ao-5" class=""><span class="file">Estudos de lógica com respostas (1 ao 5)</span></a></li>
+                                                            <li class="last"><a href="nivel-zero/estudos-de-logica-6-ao-10" class=""><span class="file">Estudos de lógica com respostas (6 ao 10)</span></a></li>
+                                                        </ul>
+                                                    </li>
+                                                </ul>
+                        -->
+
+
+                        <ul class="filetree" id="browser">
+                            <li class="folder"><span class="folder">Nível 0</span>
+                                <ul>
+                                    <li><a href="nivel-zero/um-bom-comeco" class=""><span class="file">Um bom começo</span></a></li>
+                                    <li><a href="nivel-zero/logica-de-programacao" class=""><span class="file">Lógica de Programação</span></a></li>
+                                    <li><a href="nivel-zero/antes-de-enfiar-o-pe-na-jaca" class=""><span class="file">Antes de enfiar o pé na jáca</span></a></li>
+                                    <li><a href="nivel-zero/intalando-o-ambiente" class=""><span class="file">Instalando o ambiente no estilo NEXT, NEXT, NEXT</span></a></li>
+                                    <li><a href="nivel-zero/estudos-de-logica-1-ao-5" class=""><span class="file">Estudos de lógica com respostas (1 ao 5)</span></a></li>
+                                    <li class="last"><a href="nivel-zero/estudos-de-logica-6-ao-10" class=""><span class="file">Estudos de lógica com respostas (6 ao 10)</span></a></li>
+                                </ul>
+                            </li>
+                            <li class="folder"><span class="folder">Nível 0</span>
+                                <ul>
+                                    <li><a href="nivel-zero/um-bom-comeco" class=""><span class="file">Um bom começo</span></a></li>
+                                    <li><a href="nivel-zero/logica-de-programacao" class=""><span class="file">Lógica de Programação</span></a></li>
+                                    <li><a href="nivel-zero/antes-de-enfiar-o-pe-na-jaca" class=""><span class="file">Antes de enfiar o pé na jáca</span></a></li>
+                                    <li><a href="nivel-zero/intalando-o-ambiente" class=""><span class="file">Instalando o ambiente no estilo NEXT, NEXT, NEXT</span></a></li>
+                                    <li><a href="nivel-zero/estudos-de-logica-1-ao-5" class=""><span class="file">Estudos de lógica com respostas (1 ao 5)</span></a></li>
+                                    <li class="last"><a href="nivel-zero/estudos-de-logica-6-ao-10" class=""><span class="file">Estudos de lógica com respostas (6 ao 10)</span></a></li>
+                                </ul>
+                            </li>
+                            <li class="folder"><span class="folder">Nível 0</span>
+                                <ul>
+                                    <li><a href="nivel-zero/um-bom-comeco" class=""><span class="file">Um bom começo</span></a></li>
+                                    <li><a href="nivel-zero/logica-de-programacao" class=""><span class="file">Lógica de Programação</span></a></li>
+                                    <li><a href="nivel-zero/antes-de-enfiar-o-pe-na-jaca" class=""><span class="file">Antes de enfiar o pé na jáca</span></a></li>
+                                    <li><a href="nivel-zero/intalando-o-ambiente" class=""><span class="file">Instalando o ambiente no estilo NEXT, NEXT, NEXT</span></a></li>
+                                    <li><a href="nivel-zero/estudos-de-logica-1-ao-5" class=""><span class="file">Estudos de lógica com respostas (1 ao 5)</span></a></li>
+                                    <li class="last"><a href="nivel-zero/estudos-de-logica-6-ao-10" class=""><span class="file">Estudos de lógica com respostas (6 ao 10)</span></a></li>
+                                </ul>
+                            </li>
+                            <li class="folder"><span class="folder">Nível 0</span>
+                                <ul>
                                     <li><a href="nivel-zero/um-bom-comeco" class=""><span class="file">Um bom começo</span></a></li>
                                     <li><a href="nivel-zero/logica-de-programacao" class=""><span class="file">Lógica de Programação</span></a></li>
                                     <li><a href="nivel-zero/antes-de-enfiar-o-pe-na-jaca" class=""><span class="file">Antes de enfiar o pé na jáca</span></a></li>
@@ -158,9 +203,35 @@
                                 </ul>
                             </li>
                         </ul>
+
+<!--                        <ul id="browser" class="filetree">
+                            <li><span class="folder">Folder 1</span>
+                                <ul>
+                                    <li><span class="file">Item 1.1</span></li>
+                                </ul>
+                            </li>
+                            <li><span class="folder">Folder 2</span>
+                                <ul>
+                                    <li><span class="folder">Subfolder 2.1</span>
+                                        <ul id="folder21">
+                                            <li><span class="file"><a href="#">file 2.1.1</a></span></li>
+                                            <li><span class="file">File 2.1.2</span></li>
+                                        </ul>
+                                    </li>
+                                    <li><span class="file">File 2.2</span></li>
+                                </ul>
+                            </li>
+                            <li class="closed"><span class="folder">Folder 3 (closed at start)</span>
+                                <ul>
+                                    <li><span class="file">File 3.1</span></li>
+                                </ul>
+                            </li>
+                            <li><span class="file">File 4</span></li>
+                        </ul>-->
+
                     </div>
                 </div>
-            </div>            
+            </div>
         </div><!-- container-  -->
 
         <script src="../furia/js/ga.js"></script>
