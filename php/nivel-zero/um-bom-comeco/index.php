fc6552ada9416cc70bc4ac4dff80fe10728b3cdb
Flávio Micheletti <flaviomicheletti@hotmail.com>
2012-10-29 23:43:43 -0200

    acertando o bootstrap

--- php/nivel-zero/um-bom-comeco/index.php
+++ php/nivel-zero/um-bom-comeco/index.php
@@ -4,8 +4,8 @@
         <title>Um bom começo | devfuria.com.br</title>
         <link href="../../../furia/boot/css/bootstrap.css" rel="stylesheet" type="text/css" />
         <link href="../../../furia/boot/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />
-        <link href="../../../furia/css/furia.css" rel="stylesheet" type="text/css" />
         <link href="../../../furia/treeview/jquery.treeview.css" rel="stylesheet" type="text/css" />
+        <link href="../../../furia/css/furia.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <div class="navbar navbar-inverse navbar-fixed-top">
