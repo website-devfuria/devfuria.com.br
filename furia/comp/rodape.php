b96b2e167570aaa1d3732a605009c7a0adabb5c6
Flávio Micheletti <flaviomicheletti@hotmail.com>
2012-11-14 23:16:06 -0200

    rodapé

--- furia/comp/rodape.php
+++ furia/comp/rodape.php
@@ -1,14 +1,51 @@
 <?php defined('DF') or die; ?>
-        <div class="container-fluid rodape hidden-phone">
-            <div class="row-fluid">
-                <div class="span4">
-                    a
-                </div>
-                <div class="span4">
-                    b
-                </div>
-                <div class="span4">
-                    c
-                </div>
-            </div>
-        </div> 
+<div class="container-fluid rodape hidden-phone">
+    <div class="row-fluid">
+        <div class="span4">
+            <p style="text-align: center; margin-top: 30px;">
+                <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/br/deed.en_US">
+                    <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc/3.0/br/88x31.png" />
+                </a>
+            </p>            
+            <p style="text-align: center">
+                2012
+            </p>            
+            <p style="text-align: justify;">
+                Esta licença permite que outros remixem, adaptem, e criem obras derivadas sobre a obra licenciada, sendo vedado o uso com fins comerciais. As novas obras devem conter menção ao autor nos créditos e também não podem ser usadas com fins comerciais, porém as obras derivadas não precisam ser licenciadas sob os mesmos termos desta licença.
+            </p>
+
+        </div>
+        <div class="span4" style="text-align: center">
+            <p style="text-align: center; margin-top: 30px;">
+                Design responsivo !!!
+            </p>
+            <p>
+                <a href="http://twitter.github.com/bootstrap/">
+                    <img alt="bootstrap" style="border-width:0" src="<?php echo BASE_PATH ?>furia/imagens/bootstrap.pg" />
+                </a>
+            </p>
+
+        </div>
+        <div class="span4">
+            <p style="margin-top: 30px;">
+                As matérias deste site estão no github, escritas em markdown:<br />
+                <a href="https://github.com/flaviomicheletti/devfuria">
+                    https://github.com/flaviomicheletti/devfuria
+                </a> 
+            </p>             
+            <p>
+                O código fonte do site também está no git:<br />
+                <a href="https://github.com/flaviomicheletti/www.devfuria.com.br">
+                    https://github.com/flaviomicheletti/www.devfuria.com.br
+                </a>                
+            </p>
+            <p>
+                Este site nasceu em junho de 2012. E eu havia planejado um design bem diferente do atual.
+                Caso esteja curioso em ver o design primário acesse:<br />
+                <a href="http://www.devfuria.com.br/old/">
+                   http://www.devfuria.com.br/old/
+                </a>
+            </p>
+        </div>
+    </div>
+</div> 
