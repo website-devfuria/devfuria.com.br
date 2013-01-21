03115c34eb1f8ba815872b7ef0c1842d0ab7717c
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-01-21 16:20:33 -0200

    rodapé

--- furia/comp/rodape.php
+++ furia/comp/rodape.php
@@ -6,10 +6,10 @@
                 <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/br/deed.en_US">
                     <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc/3.0/br/88x31.png" />
                 </a>
-            </p>            
+            </p>
             <p style="text-align: center">
                 2012
-            </p>            
+            </p>
             <p style="text-align: justify;">
                 Esta licença permite que outros remixem, adaptem, e criem obras derivadas sobre a obra licenciada, sendo vedado o uso com fins comerciais. As novas obras devem conter menção ao autor nos créditos e também não podem ser usadas com fins comerciais, porém as obras derivadas não precisam ser licenciadas sob os mesmos termos desta licença.
             </p>
@@ -21,26 +21,20 @@
             </p>
             <p>
                 <a href="http://twitter.github.com/bootstrap/">
-                    <img alt="bootstrap" style="border-width:0" src="<?php echo BASE_PATH ?>furia/imagens/bootstrap.pg" />
+                    <img alt="bootstrap" style="border-width:0" src="<?php echo BASE_PATH ?>furia/imagens/bootstrap.jpg" />
                 </a>
             </p>
 
         </div>
         <div class="span4">
             <p style="margin-top: 30px;">
-                As matérias deste site estão no github, escritas em markdown:<br />
-                <a href="https://github.com/flaviomicheletti/devfuria">
-                    https://github.com/flaviomicheletti/devfuria
-                </a> 
-            </p>             
-            <p>
-                O código fonte do site também está no git:<br />
+                As matérias e o código fonte deste site estão no github, escritas em markdown:<br />
                 <a href="https://github.com/flaviomicheletti/www.devfuria.com.br">
-                    https://github.com/flaviomicheletti/www.devfuria.com.br
-                </a>                
+                    https://github.com/flaviomicheletti/devfuria
+                </a>
             </p>
             <p>
-                Este site nasceu em junho de 2012. E eu havia planejado um design bem diferente do atual.
+                Este site nasceu em junho de 2012. Eu havia planejado um design bem diferente do atual.
                 Caso esteja curioso em ver o design primário acesse:<br />
                 <a href="http://www.devfuria.com.br/old/">
                    http://www.devfuria.com.br/old/
@@ -48,4 +42,4 @@
             </p>
         </div>
     </div>
-</div> 
+</div>
