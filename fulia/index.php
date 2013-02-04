2bced9429d2ee5278ef5e917e025cb0a35c6e3b1
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-02-04 17:30:17 -0200

    Fulia
    INdesrindo a ára administrativa

---
+++ fulia/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Includes
+ */
+require "../furia/includes/bs.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Furia e Fulia</title>
+        <link href="<?php echo BASE_PATH; ?>furia/boot/css/bootstrap.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo BASE_PATH; ?>furia/boot/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+
+
+    </body>
+    <script src="<?php echo BASE_PATH; ?>furia/boot/js/jquery.js" type="text/javascript"></script>
+    <script src="<?php echo BASE_PATH; ?>furia/boot/js/bootstrap.js" type="text/javascript"></script>
+    <script src="index.js" type="text/javascript"></script>
+</html>
