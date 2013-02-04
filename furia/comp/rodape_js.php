59f6568a60c264976ee5edaa3f12d09bbe7a2e6a
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-02-04 17:29:58 -0200

    /furia
    Corrigindo o rodapé dos JS

--- furia/comp/rodape_js.php
+++ furia/comp/rodape_js.php
@@ -2,10 +2,10 @@
 <script type="text/javascript">
 var DF = {
     base_path: "<?php echo BASE_PATH; ?>"
-} 
+}
 </script>
-<script src="<?php echo BASE_PATH; ?>furia/js/ga.js"></script>
-<script src="<?php echo BASE_PATH; ?>furia/boot/js/jquery.js"></script>
-<script src="<?php echo BASE_PATH; ?>furia/boot/js/bootstrap.js"></script>
-<script src="<?php echo BASE_PATH; ?>furia/treeview/jquery.treeview.js"></script>
-<script src="<?php echo BASE_PATH; ?>furia/js/furia.js"></script>
+<script src="<?php echo BASE_PATH; ?>furia/js/ga.js" type="text/javascript"></script>
+<script src="<?php echo BASE_PATH; ?>furia/boot/js/jquery.js" type="text/javascript"></script>
+<script src="<?php echo BASE_PATH; ?>furia/boot/js/bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo BASE_PATH; ?>furia/treeview/jquery.treeview.js" type="text/javascript"></script>
+<script src="<?php echo BASE_PATH; ?>furia/js/furia.js" type="text/javascript"></script>
