c8bbe8543a22e984193481943a1b4436adbd8280
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-02-08 15:14:04 -0200

    rodapé

--- furia/comp/rodape.php
+++ furia/comp/rodape.php
@@ -1,6 +1,9 @@
 <?php defined('DF') or die; ?>
 <div class="container-fluid rodape hidden-phone">
     <div class="row-fluid">
+
+        <div class="span4"></div>
+
         <div class="span4">
             <p style="text-align: center; margin-top: 30px;">
                 <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/br/deed.en_US">
@@ -13,29 +16,13 @@
             <p style="text-align: justify;">
                 Esta licença permite que outros remixem, adaptem, e criem obras derivadas sobre a obra licenciada, sendo vedado o uso com fins comerciais. As novas obras devem conter menção ao autor nos créditos e também não podem ser usadas com fins comerciais, porém as obras derivadas não precisam ser licenciadas sob os mesmos termos desta licença.
             </p>
+            <p style="text-align: center">
+                <a href="https://github.com/flaviomicheletti/www.devfuria.com.br">Forkme on github</a>
+            </p>
+
         </div>
 
-        <div class="span4">
-            <p style="margin-top: 30px;">
-                <a href="https://github.com/flaviomicheletti/www.devfuria.com.br">
-                    <img src="<?php echo BASE_PATH ?>furia/imagens/github.jpg" alt="forkme on github" title="forkme on github" />
-                </a>
-            </p>
-            <p style="text-align: center;">
-                Este site nasceu em junho de 2012. <br />
-            </p>
-        </div>
-
-        <div class="span4" style="text-align: center">
-            <p style="margin-top: 30px;>
-                <a href="http://twitter.github.com/bootstrap/">
-                    <img alt="bootstrap" style="border-width:0" src="<?php echo BASE_PATH ?>furia/imagens/bootstrap.jpg" />
-                </a>
-            </p>
-            <p style="text-align: center;">
-                Design responsivo !!!
-            </p>
-        </div>
+        <div class="span4"></div>
 
     </div>
 </div>
