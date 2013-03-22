1b0ebd8955eca606aa8ba1814b27eca3145ca1cf
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-03-22 16:51:52 -0300

    exercio 01 html

---
+++ html-css/basico/exercicio-criando-html/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Arquivo index que funciona como redirecionador
+ */
+
+
+/**
+ * Includes
+ */
+require "../../../furia/includes/bs.php";
+
+
+/**
+ * Instancia a matéria correta
+ */
+$materia = new Materia(11);
+
+
+/**
+ * View
+ */
+require BASE_PATH.VIEWS_PATH."materias.php";
+
+?>
