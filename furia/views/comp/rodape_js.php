600e80e5d085cfc74dcfe2ed1bf6484d483179fc
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-08-11 17:16:29 -0300

    desligando 'ga'

--- furia/views/comp/rodape_js.php
+++ furia/views/comp/rodape_js.php
@@ -4,7 +4,7 @@ var DF = {
     base_path: "<?php echo ROOT_PATH; ?>"
 }
 </script>
-<script src="<?php echo LINKS_PATH; ?>furia/js/ga.js" type="text/javascript"></script>
+<!--<script src="<?php // echo LINKS_PATH; ?>furia/js/ga.js" type="text/javascript"></script>-->
 <script src="<?php echo LINKS_PATH; ?>furia/biblio/jquery/jquery-1.7.2.min.js" type="text/javascript"></script>
 <script src="<?php echo LINKS_PATH; ?>furia/biblio/bootstrap/js/bootstrap.js" type="text/javascript"></script>
 <script src="<?php echo LINKS_PATH; ?>furia/biblio/treeview/jquery.treeview.js" type="text/javascript"></script>
