ca0fe3715439725e82d15c43ebb1521d12c70807
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-12-05 12:50:57 -0200

    leitura sugerida (regexp)

--- regexp/basico/new-regexp/index.php
+++ regexp/basico/new-regexp/index.php
@@ -11,7 +11,6 @@ require "../../../core/boot.php";
 <html lang="pt">
     <head>
         <?php
-        
         $core->head->setTitle('REGEXP');
         include BASE_PATH . VIEWS_PATH . "/head.php";
         ?>
@@ -19,8 +18,6 @@ require "../../../core/boot.php";
     <body>
 
         <?php
-        
-        
         $core->navtop->secao_ativa = Core::SECAO_ER;
         include BASE_PATH . VIEWS_PATH . "/nav-top.php";
         ?>
@@ -57,7 +54,7 @@ require "../../../core/boot.php";
                             <h1 id="intro">Sobre a função</h1>
                         </div>
 
-                        <p>Utilizando-se do construtor RegExp() podemos compilar a expressão regular antes de usá-la. </p>
+                        <p>Utilizando-se do construtor <code>RegExp()</code> podemos compilar a expressão regular antes de usá-la. </p>
 
                         <p>Isso é uma vantagem? Não exatamente.</p>
 
@@ -66,7 +63,7 @@ require "../../../core/boot.php";
                         <p>Uma vantagem seria o fato de ser possível criar a expressão regular apartir de uma string, isto quer dizer que
                             é possível criar a expressão de forma dinâmica.</p>
 
-                        <p>Imagine que o usuário irá digitar a palavra "exemplo" e o sistema armazenará o valor na variável <em>regexp</em>. Agora é só 
+                        <p>Imagine que o usuário irá digitar a palavra <code>"exemplo"</code> e o sistema armazenará o valor na variável <code>regexp</code>. Agora é só 
                             passar a variável para o construturor e executar a expressão.</p>
 
                         <p>Resumindo, a ER criada apartir de uma string lhe derá mair poder de flexibilidade e, obviamente, maior responsabilidade.</p>
@@ -99,48 +96,56 @@ console.log(pattern, resultado);
 
                         <p>Uma vez com a ER compilada podemos executá-la em qualquer uma das três formas já discutidas nas matérias anteirores.</p>
 
-                        <h2>Leituras adicionais sugeridas</h2>
+                        <div class="bs-docs-section">
+                            <div class="page-header">
+                                <h1 id="leituras">Leituras adicionais sugeridas</h1>
+                            </div>                        
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Internet</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="list-group">
+                                        <a href="#" class="list-group-item">
+                                            <h4 class="list-group-item-heading">Devfuria - matérias anteiores</h4>
+                                            <p class="list-group-item-text">Leia as três materias anteriores a estas falando sobre as funções exec(), teste() e match().</p>
+                                        </a>
+                                        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions?redirectlocale=en-US&redirectslug=JavaScript%2FGuide%2FRegular_Expressions" class="list-group-item" title="link-externo">
+                                            <h4 class="list-group-item-heading">Documentação Oficial</h4>
+                                            <p class="list-group-item-text">...falando sobre o objeto global RegExp. Apesar de ser um rascunho há informações
+                                                bastante precisas</p>
+                                            <span class="label label-default">http://www.php.net/manual/pt_BR/getting-started.php</span>
+                                        </a>
+                                    </div>
+                                </div>                        
+                            </div>                        
+                            <?php
+                            $core->paginacao->link_ativo = "/regexp/basico/new-regexp/";
+                            $core->paginacao->descobrirAnteriorProxima($core->links[Core::SECAO_ER]);
+                            include BASE_PATH . VIEWS_PATH . "/paginacao.php";
+                            ?>                        
+                        </div>
 
-                        <dl id="leitura_adcionais">
-                            <dt>Internet</dt>
-                            <dd>Leia as três materias anteriores a estas falando sobre as funções exec(), teste() e match()
-                                <a href="http://www.php.net" >http://www.php.net</a>
-                                <a href="http://www.php.net" >http://www.php.net</a>
-                                <a href="http://www.php.net" >http://www.php.net</a>
-                            </dd>
+                    </div><!-- Corpo da matéria -->
+                </div><!-- row -->
 
-                            <dd>Documentação Oficial falando sobre o objeto global RegExp. Apesar de ser um rascunho há informações
-                                bastante precisas
-                                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions?redirectlocale=en-US&redirectslug=JavaScript%2FGuide%2FRegular_Expressions">Doc. Oficial</a>
-                            </dd>
-                        </dl>
-                        <?php
-                        $core->paginacao->link_ativo = "/regexp/basico/new-regexp/";
-                        $core->paginacao->descobrirAnteriorProxima($core->links[Core::SECAO_ER]);
-                        include BASE_PATH . VIEWS_PATH . "/paginacao.php";
-                        ?>                        
-                    </div>
+            </div><!-- Matéria -->
 
-                </div><!-- Corpo da matéria -->
-            </div><!-- row -->
-
-        </div><!-- Matéria -->
-
-        <footer class="bs-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h1>RegExp</h1>
-                        <?php
-                        $core->lista->setLinks($core->links, Core::SECAO_ER);
-                        $core->lista->link_ativo = "/regexp/basico/new-regexp/";
-                        include BASE_PATH . VIEWS_PATH . "/lista-secao.php";
-                        ?>
+            <footer class="bs-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h1>RegExp</h1>
+                            <?php
+                            $core->lista->setLinks($core->links, Core::SECAO_ER);
+                            $core->lista->link_ativo = "/regexp/basico/new-regexp/";
+                            include BASE_PATH . VIEWS_PATH . "/lista-secao.php";
+                            ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
-        <?php include BASE_PATH . VIEWS_PATH . "/footer-js.php"; ?>
+            </footer>
+            <?php include BASE_PATH . VIEWS_PATH . "/footer-js.php"; ?>
 
     </body>
 </html>
