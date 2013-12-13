23b8b64f7131fb2d82763939888036ba70430b3d
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-12-13 12:57:28 -0200

    redireciona: lógica

--- logica/basico/exercicio01/index.php
+++ logica/basico/exercicio01/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * HTML & CSS
+ * Lógica
  */
 /**
  * Includes
@@ -11,8 +11,8 @@ require "../../../core/boot.php";
 <html lang="pt">
     <head>
         <?php
-        $core->head->setTitle('Introdução ao HTML e ao CSS');
-        $core->head->setDescription('Esta é uma página de rediracionamento');
+        $core->head->setTitle('Exercćio de lógica de programação');
+        $core->head->setDescription('Esta é uma página de redirecionamento');
         $core->head->setAuthor();
         include BASE_PATH . VIEWS_PATH . "/head.php";
         ?>
@@ -27,16 +27,16 @@ require "../../../core/boot.php";
 
 <div class="jumbotron">
   <h1>Página não encontrada!</h1>
-  <p>Esta matéria foi desmenbrada em duas: <span class="label label-info">2) Primeiro contato com HTML</span> e <span class="label label-info">3) Primeiro contato com CSS</span>.</p>
-  <p>Utilize o menu abaixo para localizar as matérias:</p>
+  <p>Esta matéria foi compactada em: <span class="label label-info">2) Primeira bateria</span>.</p>
+  <p>Utilize o menu abaixo para se localizar:</p>
 </div>
 
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6">
-                            <h3>HTML & CSS</h3>
+                            <h3>Lógica de programação</h3>
                             <?php
-                            $core->lista->setLinks($core->links, Core::SECAO_HTML);
+                            $core->lista->setLinks($core->links, Core::SECAO_LOG);
                             include BASE_PATH . VIEWS_PATH . "/lista-secao.php";
                             ?>
                         </div>
