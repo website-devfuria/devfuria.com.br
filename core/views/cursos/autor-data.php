302313384ac07c83f99e83cda3817baefcf4c40e
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-03-27 17:21:59 -0300

    acertando data na view 'autor-data.php'

--- core/views/cursos/autor-data.php
+++ core/views/cursos/autor-data.php
@@ -1,7 +1,7 @@
 <div class = "bs-old-docs">
     <div class = "container">
         <?php echo $pagina->autor; ?>,
-        atualizado em <span class = "label label-info"><?php echo $pagina->dtAtualizacao; ?></span>,
-        escrito em <span class = "label label-success"><?php echo $pagina->dtCriacao; ?></span>.
+        atualizado em <span class = "label label-info"><?php echo $pagina->getDtAtualizacao(); ?></span>,
+        escrito em <span class = "label label-success"><?php echo $pagina->getDtCriacao(); ?></span>.
     </div>
 </div>
