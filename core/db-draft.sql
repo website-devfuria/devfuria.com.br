426252e3e91418add24201179c26af0f41b2490b
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-04-11 11:53:50 -0300

    db sql

--- core/db-draft.sql
+++ core/db-draft.sql
@@ -1,6 +1,3 @@
 --
 -- Atualizações
 --
-INSERT INTO `devfuria_fulia`.`categorias` (`id`, `codigo`, `label`, `ativo`, `ordem`) VALUES ('3', '', 'Canvas', '1', '3');
-UPDATE `devfuria_fulia`.`categorias` SET `codigo` = 'canvas' WHERE `categorias`.`codigo` = '';
-ALTER TABLE `paginas` ADD `nivel` VARCHAR( 13 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'basico' AFTER `categoria` 
