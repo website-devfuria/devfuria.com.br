fcba669a23ad1089f2f8f6117c8a107b1ed46656
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-04-11 23:10:48 -0300

    db-draft

--- core/db-draft.sql
+++ core/db-draft.sql
@@ -1,31 +1,6 @@
 --
 -- Atualizações
--- (a primeira da lista é a mais atual)
 --
-UPDATE paginas SET autor = null;
-
-UPDATE `devfuria_fulia`.`paginas`
-SET `metaTitle` = 'Refatorando o código da matéria ''Code - A lâmpada'''
-WHERE `paginas`.`url` = '/js/basico/code-lampadas-refatorando/';
-
-UPDATE `devfuria_fulia`.`paginas`
-SET `metaTitle` = 'JavaScript OOP - Conceitos básicos'
-WHERE `paginas`.`url` = '/js/basico/oop-primeiro-degrau/';
-
-UPDATE `devfuria_fulia`.`paginas`
-SET `metaDescr` = 'Aprenda a debugar seus scripts em Javascript, este é o primeiro passo na depuração de código.'
-WHERE `paginas`.`url` = '/js/basico/debugando/';
-
-UPDATE `devfuria_fulia`.`paginas`
-SET `metaDescr` = 'Veremos onde colocar o JS,  OOP, FIREBUG,  a questão da segurança e separações de interesses (separation of concerns).'
-WHERE `paginas`.`url` = '/js/basico/preparando-o-terreno/';
-
-UPDATE `devfuria_fulia`.`paginas`
-SET `metaDescr` = 'Uma introdução ao Javascript, acompanha 2 script para quebrar o gelo.'
-WHERE `paginas`.`url` = '/js/basico/intro/';
-
-ALTER TABLE `paginas` ADD PRIMARY KEY ( `url` );
-
-UPDATE `devfuria_fulia`.`paginas`
-SET `metaDescr` = 'Veremos Separações de Interesses (separation of concerns), onde colocar o JS, OOP, FIREBUG e a questão da segurança!.'
-WHERE `paginas`.`url` = '/js/basico/preparando-o-terreno/';
+INSERT INTO `devfuria_fulia`.`categorias` (`id`, `codigo`, `label`, `ativo`, `ordem`) VALUES ('3', '', 'Canvas', '1', '3');
+UPDATE `devfuria_fulia`.`categorias` SET `codigo` = 'canvas' WHERE `categorias`.`codigo` = '';
+ALTER TABLE `paginas` ADD `nivel` VARCHAR( 13 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'basico' AFTER `categoria` 
