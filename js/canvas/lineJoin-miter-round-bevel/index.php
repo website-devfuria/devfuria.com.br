95f09ba2b48737d8484564aac4d8658dc1d8b637
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-04-16 10:14:13 -0300

    js/index fechando divs

--- js/canvas/lineJoin-miter-round-bevel/index.php
+++ js/canvas/lineJoin-miter-round-bevel/index.php
@@ -11,19 +11,12 @@ $view->secoes[Conteudo::SECAO_JS]['href'] = "js/?" . "canvas";
     <head>
         <?php include BASE_PATH . VIEWS_PATH . "/cursos/head.php"; ?>
         <style>
-            body {
-                background: #eeeeee;
-            }
             canvas {
                 background: #ffffff;
                 cursor: pointer;
-                margin-left: 10px;
                 -webkit-box-shadow: 4px 4px 8px rgba(0,0,0,0.5);
                 -moz-box-shadow: 4px 4px 8px rgba(0,0,0,0.5);
                 box-shadow: 4px 4px 8px rgba(0,0,0,0.5);
-            }
-            div.row {
-                margin-bottom: 50px;
             }
         </style>        
     </head>
@@ -45,17 +38,15 @@ $view->secoes[Conteudo::SECAO_JS]['href'] = "js/?" . "canvas";
                 <div class="col-md-9" role="main">
 
                     <div class="bs-docs-section">
+                        <div class="page-header">
+                            <h2>Precisamos de pelo menos um `moveTo`</h2>
+                        </div> 
 
-                        <p>Texto introdutório</p>
-                        <p>Texto introdutório</p>
-                        <p>Texto introdutório</p>
-                        <p>Texto introdutório</p>
+                        <p>Para o `lineTo` funcionar precisamos de pelo menos um `moveTo`</p>
+                        <p>Para o `lineTo` funcionar precisamos de pelo menos um `moveTo`</p>
+                        <p>Para o `lineTo` funcionar precisamos de pelo menos um `moveTo`</p>
 
                         <div class="row">
-                            <div class="page-header">
-                                <h2>Precisamos de pelo menos um `moveTo`</h2>
-                            </div>                            
-
                             <div class="col-md-5">
                                 <canvas id='ex01' width='300' height='150'>Canvas not supported</canvas>
                                 <script>
@@ -67,19 +58,27 @@ $view->secoes[Conteudo::SECAO_JS]['href'] = "js/?" . "canvas";
 
                             </div>
                             <div class="col-md-7">
-                                <p>Para o `lineTo` funcionar precisamos de pelo menos um `moveTo`</p>
+                                <p></p>
 
-                                <pre><code>var canvas = document.getElementById('ex01');
+                                <pre><code class="language-javascript">var canvas = document.getElementById('ex01');
 var context = canvas.getContext('2d');
 context.lineTo(100, 50);
 context.stroke();                                    
 </code></pre>
-                                <p>Para o `lineTo` funcionar precisamos de pelo menos um `moveTo`</p>
-                                <p>Para o `lineTo` funcionar precisamos de pelo menos um `moveTo`</p>
-                                <p>Para o `lineTo` funcionar precisamos de pelo menos um `moveTo`</p>
-
                             </div>
                         </div>                        
+                    </div>
+
+
+                    <div class="bs-docs-section">
+                        <div class="page-header">
+                            <h2>O mínimo para o `linteTo` funcionar</h2>
+                        </div> 
+
+                        <p>O mínimo para o `linteTo` funcionar</p>
+                        <p>O mínimo para o `linteTo` funcionar</p>
+                        <p>O mínimo para o `linteTo` funcionar</p>
+                        <p>O mínimo para o `linteTo` funcionar</p>
 
                         <div class="row">
                             <div class="col-md-5">
@@ -93,21 +92,32 @@ context.stroke();
                                 </script>                           
                             </div>
                             <div class="col-md-7">
-                                <p>O mínimo para o `linteTo` funcionar</p>
-                                <p>O mínimo para o `linteTo` funcionar</p>
-                                <p>O mínimo para o `linteTo` funcionar</p>
-                                <p>O mínimo para o `linteTo` funcionar</p>
-                                <p>O mínimo para o `linteTo` funcionar</p>
-                                <p>O mínimo para o `linteTo` funcionar</p>
-                                <p>O mínimo para o `linteTo` funcionar</p>
-                                <p>O mínimo para o `linteTo` funcionar</p>
+                                <pre><code class="language-javascript">var canvas = document.getElementById('ex02');
+var context = canvas.getContext('2d');
+context.moveTo(10, 10);
+context.lineTo(100, 50);
+context.stroke();                                    
+</code></pre>
                             </div>
                         </div>
+                    </div>                        
+
+
+                    <div class="bs-docs-section">
+                        <div class="page-header">
+                            <h2>um moveTo para escrever várias linhas</h2>
+                        </div>  
+
+                        <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
+                        <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
+                        <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
+                        <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
+                        <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
+                        <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
 
                         <div class="row">
                             <div class="col-md-5">
                                 <canvas id='ex03' width='300' height='150'>Canvas not supported</canvas>
-
                                 <script>
                                     var canvas = document.getElementById('ex03');
                                     var context = canvas.getContext('2d');
@@ -119,25 +129,20 @@ context.stroke();
                                 </script>                          
                             </div>
                             <div class="col-md-7">
-                                <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
+                                <pre><code class="language-javascript">var canvas = document.getElementById('ex03');
+var context = canvas.getContext('2d');
+context.moveTo(10, 10);
+context.lineTo(100, 50);
+context.lineTo(50, 110);
+context.lineTo(110, 120);
+context.stroke();                                   
+</code></pre>                                
                                 <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
                                 <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
                                 <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
                                 <p>Precisamos de apenas um moveTo para escrever várias linhas</p>
                             </div>
-                        </div>                        
-
-
-
-                        <p>
-
-                        </p>
-
-
-
-
-
-
+                        </div>                          
                     </div>
 
                 </div><!-- Corpo da matéria -->
