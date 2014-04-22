ef7fdc4be714468829d378e8fd9b33d2e6483503
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-04-22 10:09:53 -0300

    db draft

--- core/db-draft.sql
+++ core/db-draft.sql
@@ -1,3 +1,18 @@
 --
 -- Atualizações
 --
+UPDATE `devfuria_fulia`.`paginas`
+SET `metaDescr` = 'Aprenda o básico de arrays em PHP, veremos como criar, iterar(foreach e while) e funções internas.'
+WHERE `paginas`.`url` = '/php/basico/arrays-vetores-matrizes/';
+
+UPDATE `devfuria_fulia`.`paginas`
+SET `metaDescr` = 'Veremos 4 conceitos básicos: endentação, nomeação, comentários e estilos. Boas práticas essenciais e que não podem faltar em seu repertório como programador.'
+WHERE `paginas`.`url` = '/php/basico/boas-praticas/';
+
+UPDATE `devfuria_fulia`.`paginas`
+SET `metaDescr` = 'Exercícios de lógica de programação com respostas (sobre operadores de atribuição)'
+WHERE `paginas`.`url` = '/logica-de-programacao/basico/exercicios-operadores-atribuicao/';
+
+UPDATE `devfuria_fulia`.`paginas`
+SET `metaDescr` = 'Introduziremos o conceito intuitivo de programação orientada a objetos, propriedades e métodos.'
+WHERE `paginas`.`url` = '/js/basico/oop-primeiro-degrau/';
