598bc04cbbda2e64ff0d01bf06b17d850f923157
Flavio Micheletti <flaviomicheletti@hotmail.com>
2014-06-21 16:00:40 -0300

    antecessor

---
+++ _drafts/logica/antecessor-sucessor.c
@@ -0,0 +1,44 @@
+// Escreva um algoritmo para mostrar o sucessor e o antecessor de um número qualquer.
+//
+// gcc foo-01.c -o executavel.run
+#include <stdio.h>
+#include <assert.h>
+
+int main (){
+
+	int numero,
+        antecessor, sucessor;
+
+	numero = 10;
+	antecessor = numero - 1;
+	sucessor   = numero + 1;
+
+    assert(9 == antecessor);
+    assert(11 == sucessor);
+
+	return 0;
+}
+/*
+
+1
+    assert(9 == antecessor);
+    assert(11 == sucessor);
+
+2
+	int antecessor, sucessor;
+
+
+    assert(9 == antecessor);
+    assert(11 == sucessor);
+
+3
+	int antecessor, sucessor;
+
+	antecessor = 9;
+	sucessor = 11;
+
+    assert(9 == antecessor);
+    assert(11 == sucessor);
+
+
+*/
