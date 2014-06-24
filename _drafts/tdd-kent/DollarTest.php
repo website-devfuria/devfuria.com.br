db78983c769567275b69e238ce63e56b11803158
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-06-24 10:36:31 -0300

    tdd-cap4

--- _drafts/tdd-kent/DollarTest.php
+++ _drafts/tdd-kent/DollarTest.php
@@ -8,11 +8,15 @@ class DollarTest extends PHPUnit_Framework_TestCase
 
     public function testMultiplicattion(){
         $five = new Dollar(5);
+        $this->assertEquals(new Dollar(10), $five->times(2));
+        $this->assertEquals(new Dollar(15), $five->times(3));
+    }
+    
+    public function testEquality() {
+        $d = new Dollar(5);
+        $this->assertTrue($d->equals(new Dollar(5)));
         
-        $product = $five->times(2);
-        $this->assertEquals(10, $product->amount);
-        
-        $product = $five->times(3);
-        $this->assertEquals(15, $product->amount);
+        $d = new Dollar(5);
+        $this->assertFalse($d->equals(new Dollar(6)));
     }
 }
