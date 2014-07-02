680f71b002042a24f0df4e646296d1db3829741c
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-07-02 17:03:58 -0300

    draft/cpp

---
+++ _drafts/cpp/code02.cpp
@@ -0,0 +1,25 @@
+// g++ foo.cpp -o executavel.run
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    int width, height;
+public:
+    void setValues(int, int);
+
+    int area() {
+        return width * height;
+    }
+};
+
+void Rectangle::setValues(int x, int y) {
+    width = x;
+    height = y;
+}
+
+int main() {
+    Rectangle rect;
+    rect.setValues(3, 4);
+    cout << "area: " << rect.area();
+    return 0;
+}
