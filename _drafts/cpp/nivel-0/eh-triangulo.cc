26ca4260f6d0df97d909fd71dc381f79454d90de
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-07-17 08:32:49 -0300

    draft/cpp

--- _drafts/cpp/nivel-0/eh-triangulo.cc
+++ _drafts/cpp/nivel-0/eh-triangulo.cc
@@ -34,3 +34,14 @@ int main() {
     testEhTriangulo();
     return 0;
 }
+
+/*
+Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+
+    Dicas:
+    Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+    Triângulo Equilátero: três lados iguais;
+    Triângulo Isósceles: quaisquer dois lados iguais;
+    Triângulo Escaleno: três lados diferentes; 
+ * 
+ * /
