812eda60fc1ef81f808d8bea16d1fe8ebd4aaab3
Flávio Micheletti <flaviomicheletti@hotmail.com>
2014-09-29 11:18:32 -0300

    assets/js/core

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,16 +1,10 @@
-hljs.initHighlightingOnLoad();
-
 var furia = {
     init: function() {
         this.apontar_links_para_fora();
-//            this.g();
     },
     apontar_links_para_fora: function() {
         $('a[title="link-externo"]').each(function() {
             $(this).attr('target', "_blanck");
         });
-    },
-    g: function() {
-        $("#___gcse_0").addClass("hidden-phone");
     }
 }.init();
