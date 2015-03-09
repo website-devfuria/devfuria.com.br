66f065d4033b085f04df43943f4e2ad1e73f8a45
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-03-09 14:16:47 -0300

    js/backbone-exemplo-01

--- javascript/backbone-exemplo-01/index.js
+++ javascript/backbone-exemplo-01/index.js
@@ -1,72 +1,58 @@
----
-layout:      grid93-article
-title:       Backbone - exemplo
-description: 
-menu:        javascript-backbone
----
+/**
+ * Pequeno exemplo demonstrando o framework Backbone (JavaScript) 
+ * personalizado para o site devfuria.com.br
+ * 
+ * Baseado no original de http://arturadib.com/hello-backbonejs/docs/3.html
+ * 
+ */
 
-
-```html
-<table>
-    <caption><button id="add">Add list item</button></caption>
-</table>
-```
-
-
-```javascript
 //
-//
+// Model
 //
 var Item = Backbone.Model.extend({
-  defaults: {
-    part1: 'hello',
-    part2: 'world'
-  }
+    defaults: {
+        part1: 'hello',
+        part2: 'world'
+    }
 });
 
 //
-//
+// Collection
 //
 var List = Backbone.Collection.extend({
-  model: Item
+    model: Item
 });
 
 //
-//
+// View
 //
 var ListView = Backbone.View.extend({
     el: $('table'),
-
     //
+    // template
     //
-    //
-    template: _.template("<tr><td><%= id %><td><%= part1 %> <%= part2 %></td>"),
-
-
+    template: _.template("<tr><td><%= id %></td><td><%= part1 %> <%= part2 %></td></tr>"),
     events: {
         'click button#add': 'addItem'
     },
     initialize: function () {
 
-        //
-        //
-        //
+        // Ligamos o evento `add` na qual escuta cada modelo adicionado
+        // a função "appendItem"
         this.collection = new List();
         this.collection.on('add', this.appendItem, this);
 
     },
-
     addItem: function () {
         var item = new Item({
             id: (this.collection.length + 1)
         });
         this.collection.add(item);
     },
-
     appendItem: function (item) {
+        // olha como o template simplifica as coisas
         this.$el.append(this.template(item.attributes));
     }
 });
 
 var listView = new ListView();
-```
