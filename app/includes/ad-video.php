5202b75eb50fd0a43e4f153496602f730d01a6fb
Flávio Micheletti <flaviomicheletti@hotmail.com>
2018-08-06 17:50:13 -0300

    ad-video

--- app/includes/ad-video.php
+++ app/includes/ad-video.php
@@ -1,8 +1,8 @@
 <div class="alert alert-info alert-dismissible text-center" role="alert">
     <p><small>advertising space</small></p>
 
-    <h2>A sintaxe básica de Python</h2>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/WqZP7atO3SE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <h2>Respondendo a perguntas...</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/qcn1LzlG3u8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <p>Não esqueça de curtir o vídeo!!!</p>
 
 </div>
