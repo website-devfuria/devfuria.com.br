9d07ece5912662154c30c44ca31a9b8cb3e87c82
Flávio Micheletti <flaviomicheletti@hotmail.com>
2018-08-22 12:56:39 -0300

    index

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 require dirname(__FILE__) . '/app/boot.php';
 
 
-
 #
 # /
 #
@@ -32,24 +31,32 @@ $slim->get('/foo', function ($request, $response, $args) {
 #
 $slim->get('[/{uri:.*}]', function ($request, $response, $args) {
 
-    $site = $GLOBALS['site'];
-    // var_dump($site); die();
+    // $site = $GLOBALS['site'];
+    // var_dump($site); //die();
 
-    // $page = new Page('/python/index.md');
-    $page = new Page('/python/sintaxe-basica/index.md');
+    // $file = new oop\File($args['uri'], $site);
+    // var_dump($this); // die();
+
+    // $page = new \oop\Page($file);
     // var_dump($page); die();
 
-    $page->uri_str = $args['uri'];
-    $page->uri_arr = explode('/', $page->uri_str);
+    $page = oop\Page::getPage($args['uri']);
     // var_dump($page); die();
 
-    $content_parsed = $this->view->fetchFromString($page->content, ['abc' => 123]);
-    // var_dump($content_parsed); die();
+    if ($page->exist()) {
+        $content_parsed = $this->view->fetchFromString($page->content, ['abc' => 123]);
+        // var_dump($content_parsed); die();
 
-    // return $this->view->render($response, 'layouts/artigo.html', ['site' => $site, 'page' => $page, "content"  => $content_parsed]);
-    return $this->view->render($response, 'layouts/artigo+toc.html', ['site' => $site, 'page' => $page, "content"  => $content_parsed]);
+        // $layout = 'layouts/artigo.html';
+        $layout = 'layouts/artigo+toc.html';
 
-    // return $response->write('páginas');
+        return $this->view->render($response, $layout, ['site' => $site, 'page' => $page, "content"  => $content_parsed]);
+
+    } else {
+        return $response->write('página não existe');
+        // redirecionar($page->uri_str);
+    }
+
 });
 
 #
