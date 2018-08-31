b8f2ed2d6071c9a6b93a397cf8642b97affe179d
Flávio Micheletti <flaviomicheletti@hotmail.com>
2018-08-31 12:57:10 -0300

    setPrevAndNext()

--- app/oop/Page.php
+++ app/oop/Page.php
@@ -33,18 +33,18 @@ class Page {
             return null;
         }
 
-        $this->md   = file_get_contents($this->file->path->base);
+        $this->md = file_get_contents($this->file->path->base);
 
-        $parser     = new \Mni\FrontYAML\Parser();
+        $parser   = new \Mni\FrontYAML\Parser();
         // var_dump($parser); die();
 
-        $document   = $parser->parse($this->md);
+        $document = $parser->parse($this->md);
         // var_dump($document); die();
 
-        $front      = $document->getYAML();
+        $front    = $document->getYAML();
         // var_dump($front); die();
 
-        $content    = $document->getContent();
+        $content  = $document->getContent();
         // var_dump($content); die();
         // echo $content;
 
@@ -79,6 +79,37 @@ class Page {
     //     # também poderia ser: tem seção mas não tem capítulo
     // }
 
+
+    #
+    # Precisa refatorar essa desgraça...
+    #
+    function setPrevAndNext() {
+
+        $urls = array_keys($this->menu);
+
+        for ($i = 0; $i < count($urls); $i++) {
+            if ($this->file->uri->str == $urls[$i]) {
+                $url_prev = array_key_exists($i-1, $urls) ?  $urls[$i-1] : "";
+                $url_acti = $urls[$i];
+                $url_next = array_key_exists($i+1, $urls) ?  $urls[$i+1] : "";
+            }
+        }
+        // var_dump("$url_prev | $url_acti | $url_next"); die();
+
+        if (array_key_exists($url_prev, $this->menu)) {
+            $this->prev = ['url' => $url_prev, 'tittle' => $this->menu[$url_prev]];
+        } else {
+            $this->prev = ['url' => '', 'tittle' => ''];
+        }
+
+        if (array_key_exists($url_next, $this->menu)) {
+            $this->next = ['url' => $url_next, 'tittle' => $this->menu[$url_next]];
+        } else {
+            $this->next = ['url' => '', 'tittle' => ''];
+        }
+        // var_dump($this->prev, $this->next); die();
+
+    }
 
 }
 
