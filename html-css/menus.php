0b29af591bbe13f48cd5efb3745651e6bfa437f4
Flávio Micheletti <flaviomicheletti@hotmail.com>
2018-11-19 14:02:17 -0200

    html-css/menus

--- html-css/menus.php
+++ html-css/menus.php
@@ -7,31 +7,31 @@ $menu = [];
 #
 #
 $menu['html-intro'] = [
-    "../introducao/" => "Introdução",
-    "../o-que-e-html/" => "O que é HTML ?",
-    "../o-que-sao-tags-html/" => "O que são tags HTML ?",
-    "../conteudo-pre-formatado-e-texto-de-codigo/" => "Conteúdo pré-formatado (pre) e texto de código (code)",
-    "../conteudo-de-textos/" => "Conteúdo de textos (títulos, parágrafos, e negrito)",
-    "../conteudo-de-links/" => "Conteúdo de links (âncoras)",
-    "../conteudo-de-listas/" => "Conteúdo de listas (ordenadas e não ordenadas)",
-    "../conteudo-de-imagens/" => "Conteúdo de imagens",
-    "../teste-seu-conhecimento-sobre-html-01/" => "Teste seu conhecimento sobre HTML"
+    "/html-css/introducao/" => "Introdução",
+    "/html-css/o-que-e-html/" => "O que é HTML ?",
+    "/html-css/o-que-sao-tags-html/" => "O que são tags HTML ?",
+    "/html-css/conteudo-pre-formatado-e-texto-de-codigo/" => "Conteúdo pré-formatado (pre) e texto de código (code)",
+    "/html-css/conteudo-de-textos/" => "Conteúdo de textos (títulos, parágrafos, e negrito)",
+    "/html-css/conteudo-de-links/" => "Conteúdo de links (âncoras)",
+    "/html-css/conteudo-de-listas/" => "Conteúdo de listas (ordenadas e não ordenadas)",
+    "/html-css/conteudo-de-imagens/" => "Conteúdo de imagens",
+    "/html-css/teste-seu-conhecimento-sobre-html-01/" => "Teste seu conhecimento sobre HTML"
 ];
 
 #
 #
 #
 $menu['html-segundo'] = [
-    "../convidando-o-css/" => "Convidando o CSS",
-    "../e-esse-tal-de-cascata/" => "E essa tal de cascata ?",
-    "../as-regras-do-css/" => "As regras do CSS",
-    "../aplicando-css-de-forma-inline/" => "Aplicando o CSS de forma inline (local)",
-    "../aplicando-css-de-forma-embedded/" => "Aplicando o CSS de forma embedded (incorporada)",
-    "../aplicando-css-de-forma-linked/" => "Aplicando o CSS de forma linked (externa)",
-    "../estilizando-a-fonte-do-documento/" => "Estilizando a fonte do documento",
-    "../estilizando-o-texto-do-documento/" => "Estilizando o texto do documento",
-    "../estilizando-dimensionamentos-espacamentos-margens-e-bordas/" => "Estilizando dimensionamentos, espaçamentos, margens e bordas",
-    "../teste-seu-conhecimento-sobre-css-01/" => "Teste seu conhecimento sobre CSS"
+    "/html-css/convidando-o-css/" => "Convidando o CSS",
+    "/html-css/e-esse-tal-de-cascata/" => "E essa tal de cascata ?",
+    "/html-css/as-regras-do-css/" => "As regras do CSS",
+    "/html-css/aplicando-css-de-forma-inline/" => "Aplicando o CSS de forma inline (local)",
+    "/html-css/aplicando-css-de-forma-embedded/" => "Aplicando o CSS de forma embedded (incorporada)",
+    "/html-css/aplicando-css-de-forma-linked/" => "Aplicando o CSS de forma linked (externa)",
+    "/html-css/estilizando-a-fonte-do-documento/" => "Estilizando a fonte do documento",
+    "/html-css/estilizando-o-texto-do-documento/" => "Estilizando o texto do documento",
+    "/html-css/estilizando-dimensionamentos-espacamentos-margens-e-bordas/" => "Estilizando dimensionamentos, espaçamentos, margens e bordas",
+    "/html-css/teste-seu-conhecimento-sobre-css-01/" => "Teste seu conhecimento sobre CSS"
 
 ];
 
@@ -39,110 +39,110 @@ $menu['html-segundo'] = [
 #
 #
 $menu['html-aplicando-conhecimento'] = [
-    "../estrutura-basica-de-um-html/" => "Estrutura básica de um HTML",
-    "../seu-primeiro-html/" => "Seu primeiro HTML",
-    "../criando-um-html-de-primeira-qualidade/" => "Criando um HTML de primeira qualidade",
-    "../adicionando-css-ao-html/" => "Adicionando CSS ao HTML",
-    "../seu-alvo-e-o-html-sua-arma-e-o-css/" => "Seu alvo é o HTML, sua arma é o CSS",
-    "../resconstruindo-biografia-de-rasmus-ledorf/" => "Resconstruindo a biografia de Rasmus Ledorf"
+    "/html-css/estrutura-basica-de-um-html/" => "Estrutura básica de um HTML",
+    "/html-css/seu-primeiro-html/" => "Seu primeiro HTML",
+    "/html-css/criando-um-html-de-primeira-qualidade/" => "Criando um HTML de primeira qualidade",
+    "/html-css/adicionando-css-ao-html/" => "Adicionando CSS ao HTML",
+    "/html-css/seu-alvo-e-o-html-sua-arma-e-o-css/" => "Seu alvo é o HTML, sua arma é o CSS",
+    "/html-css/resconstruindo-biografia-de-rasmus-ledorf/" => "Resconstruindo a biografia de Rasmus Ledorf"
 ];
 
 #
 #
 #
 $menu['html-elementos-basicos'] = [
-    "../sintaxe-basica-do-html/" => "Sintaxe básica do HTML",
-    "../texto/" => "Texto",
-    "../ancoras-links/" => "Âncoras (links)",
-    "../listas/" => "Listas",
-    "../tabelas/" => "Tabelas"
+    "/html-css/sintaxe-basica-do-html/" => "Sintaxe básica do HTML",
+    "/html-css/texto/" => "Texto",
+    "/html-css/ancoras-links/" => "Âncoras (links)",
+    "/html-css/listas/" => "Listas",
+    "/html-css/tabelas/" => "Tabelas"
 ];
 
 #
 #
 #
 $menu['html-estilos-basicos'] = [
-    "../display/" => "display",
-    "../margin/" => "margin",
-    "../content/" => "content",
-    "../border-style/" => "border style"
+    "/html-css/display/" => "display",
+    "/html-css/margin/" => "margin",
+    "/html-css/content/" => "content",
+    "/html-css/border-style/" => "border style"
 ];
 
 #
 #
 #
 $menu['html-conceitos'] = [
-    "../o-basico-sobre-servidor-web/" => "O básico sobre Servidor Web",
-    "../caminhos-paths-absolutos-relativos-e-etc/" => "Caminhos (paths) absolutos, relativos e etc...",
-    "../seletores/" => "Seletores",
-    "../dom-document-object-model/" => "Dom - Document Object Model",
-    "../pseudoelementos/" => "Pseudoelementos",
-    "../elementos-inline-e-elementos-block-level/" => "Elementos in-line e elementos block-level"
+    "/html-css/o-basico-sobre-servidor-web/" => "O básico sobre Servidor Web",
+    "/html-css/caminhos-paths-absolutos-relativos-e-etc/" => "Caminhos (paths) absolutos, relativos e etc...",
+    "/html-css/seletores/" => "Seletores",
+    "/html-css/dom-document-object-model/" => "Dom - Document Object Model",
+    "/html-css/pseudoelementos/" => "Pseudoelementos",
+    "/html-css/elementos-inline-e-elementos-block-level/" => "Elementos in-line e elementos block-level"
 ];
 
 #
 #
 #
 $menu['html-tipografia'] = [
-    "../introducao-a-tipografia-para-a-web/" => "Introdução a Tipografia (para a Web)",
-    "../propriedades-para-fontes/" => "Propriedades para fontes",
-    "../font-family-genericas/" => "Font Family Genéricas",
-    "../font-family-seguras/" => "Font Family Seguras"
+    "/html-css/introducao-a-tipografia-para-a-web/" => "Introdução a Tipografia (para a Web)",
+    "/html-css/propriedades-para-fontes/" => "Propriedades para fontes",
+    "/html-css/font-family-genericas/" => "Font Family Genéricas",
+    "/html-css/font-family-seguras/" => "Font Family Seguras"
 ];
 
 #
 #
 #
 $menu['html-divs'] = [
-    "../modelo-de-caixa-box-model/" => "Modelo de caixa (box model)",
-    "../position/" => "position",
-    "../posicionamento-fixo/" => "Posicionamento Fixo",
-    "../flutuando-divs/" => "Flutuando divs",
-    "../criando-shapes-com-css/" => "Criando shapes com CSS"
+    "/html-css/modelo-de-caixa-box-model/" => "Modelo de caixa (box model)",
+    "/html-css/position/" => "position",
+    "/html-css/posicionamento-fixo/" => "Posicionamento Fixo",
+    "/html-css/flutuando-divs/" => "Flutuando divs",
+    "/html-css/criando-shapes-com-css/" => "Criando shapes com CSS"
 ];
 
 #
 #
 #
 $menu['html-divs-centralizando'] = [
-    "../centralizando-div-com-margem-automatica/" => "Centralizando div com margem automatica",
-    "../centralizando-div-com-margem-negativa/" => "Centralizando div com margem negativa"
+    "/html-css/centralizando-div-com-margem-automatica/" => "Centralizando div com margem automatica",
+    "/html-css/centralizando-div-com-margem-negativa/" => "Centralizando div com margem negativa"
 ];
 
 #
 #
 #
 $menu['html-imagens'] = [
-    "../tag-img/" => "tag img",
-    "../tag-figure/" => "tag figure",
-    "../background-image/" => "background image",
-    "../flutuando-imagens/" => "Flutuando imagens"
+    "/html-css/tag-img/" => "tag img",
+    "/html-css/tag-figure/" => "tag figure",
+    "/html-css/background-image/" => "background image",
+    "/html-css/flutuando-imagens/" => "Flutuando imagens"
 ];
 
 #
 #
 #
 $menu['html-layout'] = [
-    "../media-queries/" => "Media Queries",
-    "../unidade-vw-vh-vmax-vmin/" => "Unidades vw, vh, vmax e vmin",
-    "../page-background-image/" => "Page Background Image",
-    "../flex-box/" => "Flex Box",
-    "../flex-box-items/" => "Flex Box - items",
-    "../flex-box-containers/" => "Flex Box - containers",
-    "../grid-layout/" => "Grid Layout"
+    "/html-css/media-queries/" => "Media Queries",
+    "/html-css/unidade-vw-vh-vmax-vmin/" => "Unidades vw, vh, vmax e vmin",
+    "/html-css/page-background-image/" => "Page Background Image",
+    "/html-css/flex-box/" => "Flex Box",
+    "/html-css/flex-box-items/" => "Flex Box - items",
+    "/html-css/flex-box-containers/" => "Flex Box - containers",
+    "/html-css/grid-layout/" => "Grid Layout"
 ];
 
 #
 #
 #
 $menu['html-formularios'] = [
-    "../introducao-aos-formularios-web/" => "Introdução aos formulários web",
-    "../estilizacao-basica-de-um-formularios-web/" => "Estilização básica de um formulário web",
-    "../campo-textbox-textarea-e-password/" => "Campos: textbox, textarea e password",
-    "../checkbox-caixa-de-checagem/" => "Checkbox: caixa de checagem",
-    "../radio-botoes-de-%20opcao/" => "Radio: botões de opção",
-    "../combobox-caixa-de-selecao/" => "Combo box: caixa de seleção",
-    "../botoes-button-submit-reset/" => "Botões: button, submit e reset",
+    "/html-css/introducao-aos-formularios-web/" => "Introdução aos formulários web",
+    "/html-css/estilizacao-basica-de-um-formularios-web/" => "Estilização básica de um formulário web",
+    "/html-css/campo-textbox-textarea-e-password/" => "Campos: textbox, textarea e password",
+    "/html-css/checkbox-caixa-de-checagem/" => "Checkbox: caixa de checagem",
+    "/html-css/radio-botoes-de-%20opcao/" => "Radio: botões de opção",
+    "/html-css/combobox-caixa-de-selecao/" => "Combo box: caixa de seleção",
+    "/html-css/botoes-button-submit-reset/" => "Botões: button, submit e reset",
     "..-new/enviando-arquivos-input-type-file/" => "Enviando arquivos: input type file"
 ];
 
