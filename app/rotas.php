f540075e07b22bc210bf3a5ef40a6db535dac647
Flávio Micheletti <flaviomicheletti@hotmail.com>
2018-11-19 13:20:25 -0200

    /logica-de-programacao/curso-de-logica.php

--- app/rotas.php
+++ app/rotas.php
@@ -12,3 +12,9 @@ $slim->get('/app/outras/', function ($request, $response, $args) {
 
 });
 
+$slim->get('/logica-de-programacao/curso-de-logica.php', function ($request, $response, $args) {
+    $destino = "/cursos/logica-de-programacao+testes-unitarios-em-python/2edicao";
+    // var_dump($destino); die();
+    return $response->withRedirect($destino, 301);
+});
+
